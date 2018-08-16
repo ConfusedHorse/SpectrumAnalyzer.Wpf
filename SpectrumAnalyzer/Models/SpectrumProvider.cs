0029decbbcfeb6c2230938df3ea4e66e27a71f06
Jörg Flechsig <goergchen@gmail.com>
2018-08-16 23:57:01 +0200

    refactoring: cleanup

--- SpectrumAnalyzer/Models/SpectrumProvider.cs
+++ SpectrumAnalyzer/Models/SpectrumProvider.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using CSCore.DSP;
 
 namespace SpectrumAnalyzer.Models
@@ -7,7 +6,6 @@ namespace SpectrumAnalyzer.Models
     public class SpectrumProvider : FftProvider
     {
         private readonly int _sampleRate;
-        private readonly List<object> _contexts = new List<object>();
 
         public SpectrumProvider(int channels, int sampleRate, FftSize fftSize)
             : base(channels, fftSize)
@@ -31,29 +29,6 @@ namespace SpectrumAnalyzer.Models
             var f = _sampleRate / 2.0;
             // ReSharper disable once PossibleLossOfFraction
             return (int)(fftBandIndex * f / (fftSize / 2));
-        }
-
-        public bool GetFftData(float[] fftResultBuffer, object context)
-        {
-            if (_contexts.Contains(context))
-                return false;
-
-            _contexts.Add(context);
-            GetFftData(fftResultBuffer);
-            return true;
-        }
-
-        public override void Add(float[] samples, int count)
-        {
-            base.Add(samples, count);
-            if (count > 0)
-                _contexts.Clear();
-        }
-
-        public override void Add(float left, float right)
-        {
-            base.Add(left, right);
-            _contexts.Clear();
         }
     }
 }
